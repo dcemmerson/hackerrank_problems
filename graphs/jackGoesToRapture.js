374d7668434247cfc6e1e0aeefe3148436c9a20d
dcemmerson <emmersod@oregonstate.edu>
2020-12-08 10:44:17 -0800

    Jack goes to rapture implemented with heap.

--- graphs/jackGoesToRapture.js
+++ graphs/jackGoesToRapture.js
@@ -4,7 +4,7 @@ function getCost(gNodes, gFrom, gTo, gWeight) {
 
     const distance = graph.getCost();
 
-    return distance === Number.MAX_SAFE_INTEGER ? 'NO PATH EXISTS' : distance;
+    console.log(distance === Number.MAX_SAFE_INTEGER ? 'NO PATH EXISTS' : distance);
 }
 
 class Graph {
@@ -26,17 +26,17 @@ class Graph {
     }
 
     getCost() {
-        const queue = []; // Use shift and push here
+        const heap = new Heap();
 
         const startVertex = this.vertices[0];
         startVertex.distance = 0;
         startVertex.found = true;
 
-        queue.push(startVertex);
+        heap.add(startVertex);
 
-        while(queue.length > 0) {
-            queue.sort(compareVertices); // Sort every iteration to mock a priority queue here.
-            const currVertex = queue.shift();
+        while(heap.length > 0) {
+            // queue.sort(compareVertices); // Sort every iteration to mock a priority queue here.
+            const currVertex = heap.getMin();
             currVertex.visited = true;
 
             currVertex.edges.forEach(e => {
@@ -45,7 +45,10 @@ class Graph {
                         (e.weight - currVertex.distance) > 0 ? e.weight : currVertex.distance);
                     if(!e.vertexA.found) {
                         e.vertexA.found = true;
-                        queue.push(e.vertexA);
+                        heap.add(e.vertexA);
+                    }
+                    else {
+                        heap.bubbleUp(e.vertexA.index);
                     }
                 }
                 else if(!e.vertexB.visited) {
@@ -53,7 +56,10 @@ class Graph {
                         (e.weight - currVertex.distance) > 0 ? e.weight : currVertex.distance);
                     if(!e.vertexB.found) {
                         e.vertexB.found = true;
-                        queue.push(e.vertexB);
+                        heap.add(e.vertexB);
+                    }
+                    else {
+                        heap.bubbleUp(e.vertexA.index);
                     }
                 }
             }); // forEach
@@ -85,20 +91,116 @@ class Vertex {
     }
 }
 
-function compareVertices(a, b) {
-    if(a.distance < b.distance) {
-        return -1;
+class Heap {
+    constructor() {
+        this.vertices = [];
+        this.count = 0;
+        this.capacity = 0;
     }
-    else if(a.distance === b.distance) {
-        return 0;
+
+    get length() {
+        return this.count;
     }
-    else {
-        return 1;
+
+    add(v) {
+        if(this.count >= this.capacity) {
+            const realloced = new Array(2 * this.capacity + 1);
+            this.capacity = 2 * this.capacity + 1;
+            this.vertices.forEach((v, index) => realloced[index] = v);
+            this.vertices = realloced;
+        }
+        v.index = this.count;
+        this.vertices[this.count] = v;
+        this.count++;
+        this.bubbleUp(this.count - 1);
+
+    }
+
+    getMin() {
+        if(this.count <= 0) {
+            return null;
+        }
+
+        const minVertex = this.vertices[0];
+        this.vertices[0] = this.vertices[this.count - 1];
+        this.vertices[0].index = 0;
+        this.vertices[this.count - 1] = null;
+        this.count--;
+        if(this.count > 0) {
+            this.trickleDown();
+        }
+
+        return minVertex;
+    }
+
+    trickleDown() {
+        let currIndex = 0;
+        let currVertex = this.vertices[currIndex];
+
+        let lChildIndex = 2 * currIndex + 1;
+        let rChildIndex = 2 * currIndex + 2;
+
+        while(lChildIndex < this.count - 1) {
+            const minChildIndex = this.vertices[lChildIndex].distance < this.vertices[rChildIndex].distance 
+                                    ? lChildIndex : rChildIndex;
+            const minChildVertex = this.vertices[minChildIndex];
+
+            if(minChildVertex.distance < currVertex.distance) {
+                this.swap(currIndex, minChildIndex);
+
+            }
+
+            currIndex = minChildIndex;
+            lChildIndex = 2 * currIndex + 1;
+            rChildIndex = 2 * currIndex + 2;
+        }
+    }
+
+    bubbleUp(startIndex) {
+        let currIndex = startIndex;
+        let parentIndex = Math.floor((currIndex - 1) / 2);
+
+        let currVertex = this.vertices[currIndex];
+        let parentVertex = this.vertices[parentIndex];
+
+        while(parentIndex >= 0 && currVertex.distance < parentVertex.distance) {
+            this.swap(currIndex, parentIndex);
+            currIndex = parentIndex;
+            currVertex = this.vertices[currIndex];
+            parentIndex = Math.floor((currIndex - 1) / 2);
+            if(parentIndex >= 0) {
+                parentVertex = this.vertices[parentIndex];
+            }
+        }
+    }
+
+    swap(indexA, indexB) {
+        const vertexA = this.vertices[indexA];
+        const vertexB = this.vertices[indexB];
+
+        this.vertices[indexA] = vertexB;
+        vertexB.index = indexA;
+
+        this.vertices[indexB] = vertexA;
+        vertexA.index = indexB;
     }
 }
 
+// function compareVertices(a, b) {
+//     if(a.distance < b.distance) {
+//         return -1;
+//     }
+//     else if(a.distance === b.distance) {
+//         return 0;
+//     }
+//     else {
+//         return 1;
+//     }
+// }
+
+
 // console.log(getCost(5, [1, 2, 3, 4, 1, 3], [2, 3, 4, 5, 3, 5], [30, 50, 70, 90, 70, 80]));
 
-// console.log(getCost(5, [1, 3, 1, 4, 2], [2, 5, 4, 5, 3], [60, 70, 120, 150, 80]));
+getCost(5, [1, 3, 1, 4, 2], [2, 5, 4, 5, 3], [60, 70, 120, 150, 80]);
 
-console.log(getCost(10, [1, 1, 1, 1, 1, 1, 1, 1], ))
+// console.log(getCost(10, [1, 1, 1, 1, 1, 1, 1, 1], ))
--- search/gridlandMetro.js
+++ search/gridlandMetro.js
@@ -1,63 +1,72 @@
 function gridlandMetro(n, m, k, track) {
-    const trackMap = arrToMap(track);
-    let count = 0;
-    for(let j = 0; j < m; j++) {
-        
-        if(!track[0]) {
-            // If there are no more tracks, take shortcut to sum
-            // upremaining rows.
-            count += (m - j) * n;
-            j = m;
+    const trackMap = arrToSquashedMap(track);
+    let count = BigInt(m * n);
+    let subtract = 0;
+    trackMap.forEach((tracks/*, key*/) => {
+        // let currCount = m;
+        for(let i = 0; i < tracks.length; i++) {
+            const currTrack = tracks[i];
+            subtract += (currTrack[2] - currTrack[1] + 1);
         }
-        else if(trackMap.get(j + 1)) {
-            const currTracks = trackMap.get(j + 1);
-            const countFilled = countWithSquashedTracks(currTracks, n);
-            // const countFilled = squashedTracks.reduce((acc, curr) => acc + (curr[2] - curr[1]), 0);
-            count += n - countFilled;
-        }
-        else {
-            count += n;
-        }
-    }
+    });
 
-    return count;
+    return Number(count - BigInt(subtract));
 }
 
-function countWithSquashedTracks(tracks, n) {
-    let count = 0;
 
-    for(let j = 0; j < tracks.length; j++) {
-        const currTrack = tracks[j];
-        let left = currTrack[1];
-        let right = currTrack[2];
-
-        for(let i = 0; i < j - 1; i++) {
-            if(left > tracks[i][1] && left < tracks[i][2]) {
-                left = tracks[i][2];
-            }
-            if(right > tracks[i][1] && right < tracks[i][2]) {
-                left = tracks[i][2];
-            }
-        }
-    }
-
-    return count;
-}
-
-function arrToMap(tracks) {
+function arrToSquashedMap(tracks) {
     const map = new Map();
 
-    tracks.forEach(tr => {
-        if(!map.get(tr[0])) {
-            map.set(tr[0], []);
+    tracks.forEach(addTrack => {
+        if(!map.get(addTrack[0])) {
+            map.set(addTrack[0], []);
+            map.get(addTrack[0]).push(addTrack);
+        }
+        else {
+            const tracks = map.get(addTrack[0]);
+            const squashedTracks = [];
+            let leftIndex = Number.MAX_SAFE_INTEGER;
+            let rightIndex = -1;
+            let ignoreAddTrack = false;
+            for(let i = 0; i < tracks.length; i++) {
+                const currTrack = tracks[i];
+                if(addTrack[1] <= currTrack[1] && addTrack[2] >= currTrack[1]) {
+                    leftIndex = Math.min(leftIndex, addTrack[1]);
+                    rightIndex = Math.max(rightIndex, addTrack[2], currTrack[2]);
+                }
+                else if(addTrack[1] <= currTrack[2] && addTrack[2] >= currTrack[2]) {
+                    rightIndex = Math.max(rightIndex, addTrack[2]);
+                    leftIndex = Math.min(leftIndex, addTrack[1], currTrack[1]);
+                }
+                else if(addTrack[1] >= currTrack[1] && addTrack[2] <= currTrack[2]) {
+                    // ignore addTrack, but keep currTrack - add track is inside currTrack
+                    squashedTracks.push(currTrack);
+                    ignoreAddTrack = true;
+                }
+                else {
+                    squashedTracks.push(currTrack);
+                }
+            }
+
+            if(rightIndex !== -1) {
+                squashedTracks.push([addTrack[0], leftIndex, rightIndex]);
+            }
+            else if(!ignoreAddTrack) {
+                squashedTracks.push(addTrack);
+            }
+            map.set(addTrack[0], squashedTracks);
+
         }
 
-        map.get(tr[0]).push(tr);
     });
 
     return map;
 }
 
-console.log(gridlandMetro(4, 4, 3, [[2, 2, 3], [3, 1, 4], [4, 4, 4]]));
+console.log(gridlandMetro(4, 4, 3, [[2, 2, 3], [3, 1, 4], [4, 4, 4],[4, 4, 4]]));
 console.log(gridlandMetro(1, 5, 3, [[1, 1, 2], [1, 2, 4], [1, 3, 5]]));
+// console.log(gridlandMetro(3, 7, 4, [[1, 1, 3], [2, 2, 2], [3, 1, 6], [3, 6, 6]]));
+console.log(gridlandMetro(2, 9, 3, [[2, 1, 5], [2, 2, 4], [2, 8, 8]]));
+console.log(gridlandMetro(2, 9, 3, [[2, 2, 4], [2, 1, 5], [2, 8, 8]]));
+
 
